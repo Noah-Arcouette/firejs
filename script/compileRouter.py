efb86f68007cae78fa8883bc603e73424f9d830b
Noah Arcouette @G5-5000 <kcu9626@gmail.com>
2023-09-25 21:56:42 -0600

    fix router spaces

--- script/compileRouter.py
+++ script/compileRouter.py
@@ -1,6 +1,7 @@
 #!/bin/python3
 from os      import stat
 from os.path import exists
+import re
 
 if exists("./router.js"):
 	if stat("./routes").st_mtime < stat("./router.js").st_mtime:
@@ -8,7 +9,7 @@ if exists("./router.js"):
 		exit(0)
 
 with open("./routes", "r") as f:
-	data = f.read()
+	data = re.sub('[ \t]+', ' ', f.read())
 
 out = """const system = require("./system.js");
 
@@ -22,8 +23,8 @@ for route in data.split("\n"):
 		print("\t".join(section))
 
 		out += f"\tapp.{section[0].lower()}(\"{section[1]}\", "
-		out += "(req, res)=>\n{"
-		out += f"\tsystem.{section[2]}(req, res)\n"
+		out += "async (req, res)=>\n{"
+		out += f"\tawait system.{section[2]}(req, res)\n"
 		out += "\tres.send(res.fakeData);\n"
 		out += "});\n";
 
